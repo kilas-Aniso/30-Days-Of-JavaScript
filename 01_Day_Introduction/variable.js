1188e7175e166dcdc57b9cdb33bf25c90de36e5e
Kilas-aniso <kilasaniso@gmail.com>
2023-06-19 11:54:38 +0300

    explored on variables and their different data types

--- 01_Day_Introduction/variable.js
+++ 01_Day_Introduction/variable.js
@@ -5,13 +5,28 @@ let country = 'Finland' // country
 let city = 'Helsinki' // capital city
 let age = 100 // age in years
 let isMarried = true
+let studentName = 'Grace'
+let studentage = 20
+let studentgrade = "Bplus"
+let studentCountry = "Kenyans"
+let isPassed = true
+
 
 // Declaring variables with number values
 const gravity = 9.81 // earth gravity  in m/s2
 const boilingPoint = 100 // water boiling point, temperature in oC
 const PI = 3.14 // geometrical constant
+const x = 2.3
+
 
 // Variables can also be declaring in one line separated by comma
 let name = 'Asabeneh', //name of a person
   job = 'teacher',
   live = 'Finland'
+
+
+let names = "Eunice Mjera", 
+          title = "Doctor",
+          country1 = "Kenyan"
+
+
